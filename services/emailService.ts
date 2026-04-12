21ec0f54aa183d567f6793753be1ab338fc5e28d
skammshik339 <mozartrumpet@gmail.com>
2026-04-12 10:44:40 +0300

    mailer

--- services/emailService.ts
+++ services/emailService.ts
@@ -1,28 +1,26 @@
-import nodemailer from "nodemailer";
-
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST || "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
+import axios from "axios";
 
 export const sendVerificationEmail = async (email: string, token: string) => {
   const verificationUrl = `https://server-from-project-react-native.onrender.com/api/auth/verify-email?token=${token}`;
 
-  await transporter.sendMail({
-    from: `"Your App" <${process.env.SMTP_USER}>`,
-    to: email,
-    subject: "Подтверждение email",
-    html: `
-    <h2>Подтвердите email</h2>
-    <p>перейдите по ссылке для подтверждения email: <a href="${verificationUrl}">${verificationUrl}</a></p>
-    `,
-  });
+  await axios.post(
+    "https://api.mailersend.com/v1/email",
+    {
+      from: { email: process.env.MAILERSEND_FROM },
+      to: [{ email }],
+      subject: "Подтверждение email",
+      html: `
+        <h2>Подтвердите email</h2>
+        <p>Перейдите по ссылке для подтверждения email:</p>
+        <a href="${verificationUrl}">${verificationUrl}</a>
+      `,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.MAILERSEND_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
 };
+
